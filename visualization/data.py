e5a829b6c087d906d5b7cb6aaadc516beab6a842
tanishq vyas <tanishqvyas069@gmail.com>
2019-09-10 12:09:08 +0530

    Refactored and file bug fixes

--- visualization/data.py
+++ visualization/data.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import os
+import shutil
 
 class Data:
 
@@ -30,44 +31,42 @@ class Data:
 	def __init__(self):
 		pass
 
+
+	# Function to add NaN
+	def add_nan(self,df,column_list,percentage):
+
+		# Adding NaN
+		for col in column_list:
+		    ori_rat = df[col].isna().mean()
+
+		    if ori_rat >= percentage: continue
+
+		    add_miss_rat = (percentage - ori_rat) / (1 - ori_rat)
+		    vals_to_nan = df[col].dropna().sample(frac=add_miss_rat).index
+		    df.loc[vals_to_nan, col] = np.NaN
+
+
+
+
 	# Function to introduce NaN values
 	def introduce_nan(self):
 	
 		# Creating Data Frame
 		df = pd.DataFrame(pd.read_csv(self.initial_data_path))
 		
-		# Adding NaN
-		for col in self.column_list1:
-		    ori_rat = df[col].isna().mean()
-
-		    if ori_rat >= self.percentage_nan[0]: continue
-
-		    add_miss_rat = (self.percentage_nan[0] - ori_rat) / (1 - ori_rat)
-		    vals_to_nan = df[col].dropna().sample(frac=add_miss_rat).index
-		    df.loc[vals_to_nan, col] = np.NaN
-
-		for col in self.column_list2:
-		    ori_rat = df[col].isna().mean()
-
-		    if ori_rat >= self.percentage_nan[1]: continue
-
-		    add_miss_rat = (self.percentage_nan[1] - ori_rat) / (1 - ori_rat)
-		    vals_to_nan = df[col].dropna().sample(frac=add_miss_rat).index
-		    df.loc[vals_to_nan, col] = np.NaN
-
-		for col in self.column_list3:
-		    ori_rat = df[col].isna().mean()
-
-		    if ori_rat >= self.percentage_nan[2]: continue
-
-		    add_miss_rat = (self.percentage_nan[2] - ori_rat) / (1 - ori_rat)
-		    vals_to_nan = df[col].dropna().sample(frac=add_miss_rat).index
-		    df.loc[vals_to_nan, col] = np.NaN
+		# Calling add_nan function to modify 3 lists of columns
+		self.add_nan(df,self.column_list1,self.percentage_nan[0])
+		self.add_nan(df,self.column_list2,self.percentage_nan[1])
+		self.add_nan(df,self.column_list3,self.percentage_nan[2])
 
 		# Filling NaN
 		df.fillna("NaN", inplace = True)
 
 		# Saving the new dataset
+
+		if os.path.exists(self.nan_directory_path):
+			shutil.rmtree(self.nan_directory_path)
+
 		os.mkdir(self.nan_directory_path) # making new directory to save newversion
 		df.to_csv(self.nan_data_path)
 		print("done") 
