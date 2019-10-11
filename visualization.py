167483639fd5c6b7c76761f409b17735aaefced8
tanishq vyas <tanishqvyas069@gmail.com>
2019-10-11 22:28:33 +0530

    showcase for phase 1

--- visualization.py
+++ visualization.py
@@ -15,17 +15,33 @@ data = manipulator.Data()
 # Introducing and replacing NaNs
 data.introduce_nan()
 data.replace_nan()
-data.plot_boxPlot(["G1","G2"],"title ye rha bhai","xlabel bhi lele", "ha y bhi le hi le", True)
-data.plot_piechart("guardian","Skadoosh")
-data.plot_histogram("G1","Marks Distro", "Marks", "Num of Students")
-data.plot_bargraph("traveltime","traveltme", "time", "Num of Students")
-data.plot_scatterPlot("G1","G2","This is dotted broo","G1","G2")
 
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>LEAVE THE SECTION BELOW>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#data.buildTrainSavePredictModel(['G3','G2','traveltime','studytime','freetime'],"G3",1)
-#####data.plot_normalProbabilityPlot("G2")
-#####data.binomial_distribution(50,0.9)
-#####data.poisson(27, 5, 100, 20, 60)
+# visualization
+data.plot_boxPlot(["G1","G2"],"Test 1 marks vs Test 2 marks","Test 1 marks", "Test 2 marks", True)
 
+data.plot_piechart("guardian","Guardians")
 
-#data.normalize(["G1"])
+data.plot_histogram("G2","Marks Distribution", "Test 2 marks", "Number of Students")
+
+data.plot_histogram("G1","Marks Distribution", "Test 1 marks", "Number of Students")
+
+data.plot_bargraph("traveltime","traveltime", "time", "Number of Students")
+
+data.plot_bargraph("reason","reason for joining school","reasons","number of students")
+
+data.plot_scatterPlot("G1","G2","Sactter Plot for marks for both tests","Test 1 marks","Test 2 marks")
+
+data.plot_scatterPlot("Mjob", "G2", "Scatter Plot for marks vs Mother's occupation","Occupations", "Test 2 marks")
+
+data.plot_scatterPlot("Fjob", "G2", "Scatter Plot for marks vs Father's occupation","Occupations", "Test 2 marks")
+
+data.plot_scatterPlot("romantic","G2","Scatter Plot for marks vs Realationship status","Is in a Realationship?","Test 2 Marks")
+
+data.plot_scatterPlot("traveltime", "G2", "Scatter Plot for marks vs traveltime","traveltime in hours", "Test 2 marks")
+
+data.plot_scatterPlot("Walc","G2","Weekly alcohol consumption vs Marks in Test 2","Weekly alcohol consumption","Test 2 marks")
+
+data.plot_scatterPlot("Dalc","G2","Daily alcohol consumption vs Marks in Test 2","Daily alcohol consumption","Test 2 marks")
+
+data.plot_scatterPlot("studytime","G2","Study time vs marks","Studytime in hrs","Test 2 marks")
+#data.plot_normalProbabilityPlot("G1")
